21f769134a791ed9f5ab8a30bf1d5368332d2a6d
deliapater <delia.paternina@gmail.com>
2023-04-27 09:33:17 +0100

    adds styling to the search bar

--- resources/js/components/TurbineInspection/TurbineInspectionTable.js
+++ resources/js/components/TurbineInspection/TurbineInspectionTable.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const gradeLabels = [
     "Perfect",
@@ -19,23 +21,29 @@ const TurbineInspectionTable = () => {
     };
 
     useEffect(() => {
-        axios.get('/api/turbine_inspections')
-          .then(response => {
-            setTurbineInspections(response.data);
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      }, []);
+        axios
+            .get("/api/turbine_inspections")
+            .then((response) => {
+                setTurbineInspections(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
     useEffect(() => {
-        const filteredInspections = turbineInspections.filter(turbineInspection =>
-          turbineInspection.turbine?.name.toLowerCase().includes(searchInput.toLowerCase()) ||
-          turbineInspection.component?.name.toLowerCase().includes(searchInput.toLowerCase())
+        const filteredInspections = turbineInspections.filter(
+            (turbineInspection) =>
+                turbineInspection.turbine?.name
+                    .toLowerCase()
+                    .includes(searchInput.toLowerCase()) ||
+                turbineInspection.component?.name
+                    .toLowerCase()
+                    .includes(searchInput.toLowerCase())
         );
         setSearchResults(filteredInspections);
-      }, [searchInput, turbineInspections]);
-    
+    }, [searchInput, turbineInspections]);
+
     if (!turbineInspections) {
         return <div>Loading...</div>;
     }
@@ -45,14 +53,22 @@ const TurbineInspectionTable = () => {
             <h1 className="text-3xl font-bold mb-4 text-center">
                 Turbine Inspections
             </h1>
-            <input
-                type="text"
-                value={searchInput}
-                onChange={handleSearchInputChange}
-                placeholder="Search for a Turbine or Component Name ..."
-                className="px-4 py-2 mb-4 rounded w-full"
-            />
-            <table className="w-full rounded">
+            <div className="relative mb-4">
+                <input
+                    type="text"
+                    value={searchInput}
+                    placeholder="Search for a Turbine or Component Name ..."
+                    className="border border-gray-300 rounded-full py-2 px-6 pl-12 pr-8 text-sm text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 w-1/4"
+                    onChange={handleSearchInputChange}
+                />
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <FontAwesomeIcon
+                        icon={faSearch}
+                        className="text-gray-500"
+                    />
+                </div>
+            </div>
+            <table className="w-full rounded-full">
                 <thead>
                     <tr>
                         <th className="bg-gray-100 border text-left px-8 py-4">
